9afe10a2ce09a6f3ec0aaa0801b3d0c85d4bd4f9
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2024-02-29 07:30:18 -0500

    Update hero list component and app component templates

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  public heroeNames: string[] = ['ironman', 'spiderman', 'thor', 'hulk', 'captain america'];
+  public deleteHeroe?: string;
+
+  removeLastHero():void {
+    this.deleteHeroe = this.heroeNames.pop();
+  }
 
 }
