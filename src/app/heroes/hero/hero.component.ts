22cee360c673d105754ed540e82cd5e86c00a9cc
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2024-02-29 06:42:13 -0500

    Refactor hero component and update dependencies

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 35;
 
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} is ${this.age} years old`;
+  }
+
+  changeHero(): void {
+    this.name = 'spiderman';
+  }
+
+  changeAge():void{
+    this.age = 25;
+  }
 }
