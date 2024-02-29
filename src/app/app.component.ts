dc27c512544bae04cf19eb2c10050900d994e776
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2024-02-29 05:41:12 -0500

    Update angular.json and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi primer proyecto en Angular';
+  public counter : number = 13;
+
+  increment(value: number): void {
+    this.counter += value;
+  }
+
+  decrement(value: number): void {
+    this.counter -= value;
+  }
+
+  reset(): void {
+    this.counter = 10;
+  }
 }
